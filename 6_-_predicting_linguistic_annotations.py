e15890e5556e5b3b8d88243015cb3c610cf76fab
Nic <thisisnic@gmail.com>
2019-11-18 10:20:54 +0000

    Add prediction notes

---
+++ 6_-_predicting_linguistic_annotations.py
@@ -0,0 +1,25 @@
+# here, we are using the model to predict the part of speech and dependencies
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+text = "It’s official: Apple is the first U.S. public company to reach a $1 trillion market value"
+
+# Process the text
+doc = nlp(text)
+
+for token in doc:
+    # Get the token text, part-of-speech tag and dependency label
+    token_text = token.text
+    token_pos = token.pos_
+    token_dep = token.dep_
+
+
+    # This is for formatting only
+    print("{:<12}{:<10}{:<10}".format(token_text, token_pos, token_dep))
+
+# You can also use spacy.explain for more info on what something is...
+spacy.explain('relcl')
+
+
+
